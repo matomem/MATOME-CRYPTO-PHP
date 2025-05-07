290abccce10c14c9ce130a5408c817377a030a72
Joseph MATOME MALATJI <82323957+matomem@users.noreply.github.com>
2025-05-07 21:52:33 +0200

    Delete matomecrypto directory

--- matomecrypto/app/views/wallet/withdraw.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html>
-<head><title>Withdraw</title></head>
-<body>
-<h1>Withdraw</h1>
-<p>To withdraw funds, please use your Luno account dashboard. Withdrawals are handled directly by Luno for security.</p>
-<a href="/wallet">Back to Wallet</a>
-</body>
-</html> 
